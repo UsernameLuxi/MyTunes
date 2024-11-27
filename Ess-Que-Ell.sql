7fc07a12624e4ae4492504372aaa23a04f0c883f
Casper <casfro01@easv365.dk>
2024-11-27 10:00:07 +0100

    fixed error hihi

--- Ess-Que-Ell.sql
+++ Ess-Que-Ell.sql
@@ -1,1 +1,38 @@
 -- SQL file containing the structure of the database. This file is for recreating the database only
+USE master;
+GO
+IF EXISTS(SELECT * FROM sys.databases WHERE name = 'DB_NAME')
+BEGIN
+  DROP DATABASE DB_NAME;
+END
+
+CREATE DATABASE DB_NAME;
+GO
+USE DB_NAME;
+GO
+
+CREATE TABLE Songs(
+    Id INT NOT NULL IDENTITY (1,1) PRIMARY KEY,
+    Title NVARCHAR(500) NOT NULL,
+    ArtistID INT NOT NULL fk,
+
+    URL NVARCHAR(1500) NOT NULL
+);
+
+CREATE TABLE Artist(
+    Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    ArtistName NVARCHAR(500) NOT NULL
+);
+
+CREATE TABLE Playlists(
+    Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    PlaylistName NVARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Genre(
+
+);
+
+CREATE TABLE PlaylistContainer(
+
+);
