be91434908feeff61eb8f01972fdab3b08606e03
Casper <casfro01@easv365.dk>
2024-11-27 20:33:55 +0100

    create a song

--- src/main/java/com/gruppe5/MyTunes/DAL/DAO_DB.java
+++ src/main/java/com/gruppe5/MyTunes/DAL/DAO_DB.java
@@ -3,10 +3,7 @@ package com.gruppe5.MyTunes.DAL;
 import com.gruppe5.MyTunes.BE.Playlist;
 import com.gruppe5.MyTunes.BE.Song;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Time;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -103,7 +100,75 @@ public class DAO_DB  implements IDataAccess{
 
     @Override
     public Song addSong(Song song) throws Exception{
-        return null;
+        String sql_insert = "INSERT INTO Songs (Title, ArtistID, Duration, GenreID, URL) VALUES (?, ?, ?, ?, ?)";
+        String sql_artist = "SELECT Artist.Id, Artist.ArtistName FROM Artist WHERE Artist.Name = ?";
+        String sql_genre = "SELECT Genre.Id, Genre.GenreName FROM Genre WHERE Genre.Name = ?";
+
+        try(Connection conn = new DBConnector().getConnection();
+            PreparedStatement ps_insert = conn.prepareStatement(sql_insert);
+            PreparedStatement ps_searchA = conn.prepareStatement(sql_artist);
+            PreparedStatement ps_searchG = conn.prepareStatement(sql_genre)){
+
+            // search the artist
+            ps_searchA.setString(1, song.getArtist().trim());
+            ResultSet aSearch = ps_searchA.executeQuery();
+            if (aSearch.next()){
+                ps_insert.setInt(2, aSearch.getInt("ArtistID"));
+            }
+            else{
+                // create the artist in the database and give id
+                PreparedStatement cArtist = conn.prepareStatement("INSERT INTO Artist (ArtistName) VALUES (?)");
+                cArtist.setString(1, song.getArtist().trim());
+                cArtist.executeUpdate();
+
+                // get the artist id
+                PreparedStatement gArtist = conn.prepareStatement("SELECT Artist.Id FROM Artist WHERE Artist.ArtistName = ?");
+                gArtist.setString(1, song.getArtist().trim());
+                ResultSet rs = gArtist.executeQuery();
+                rs.next();
+                ps_insert.setInt(2, rs.getInt("Id"));
+            }
+
+            // search the genre
+            ps_searchG.setString(1, song.getGenre().trim());
+            ResultSet gSearch = ps_searchG.executeQuery();
+            if (gSearch.next()){
+                ps_insert.setInt(4, gSearch.getInt("Id"));
+            }
+            else{
+                // create the genre in the database
+                PreparedStatement cGenre = conn.prepareStatement("INSERT INTO Genre (GenreName) VALUES (?)");
+                cGenre.setString(1, song.getGenre().trim());
+                cGenre.executeUpdate();
+
+                // get the artist id
+                PreparedStatement gGenre = conn.prepareStatement("SELECT Genre.Id FROM Genre WHERE Genre.GenreName = ?");
+                gGenre.setString(1, song.getArtist().trim());
+                ResultSet rs = gGenre.executeQuery();
+                rs.next();
+                ps_insert.setInt(4, rs.getInt("Id"));
+            }
+
+            // fill out the rest of the information
+            ps_insert.setString(1, song.getTitle().trim());
+            ps_insert.setTime(3, song.getDuration());
+            ps_insert.setString(5, song.getURL());
+
+            ps_insert.executeUpdate();
+
+            // get the id
+            PreparedStatement ps = conn.prepareStatement("SELECT Songs.Id FROM Songs WHERE Songs.Title = ? AND Songs.Duration = ? AND Songs.URL = ?");
+            ps.setString(1, song.getTitle().trim());
+            ps.setTime(2, song.getDuration());
+            ps.setString(3, song.getURL());
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            return new Song(rs.getInt("Id"), song.getTitle(), song.getArtist(), song.getDuration(), song.getGenre(), song.getURL());
+
+        }
+        catch(Exception e){
+            throw new Exception("Unable to create the song " + song.getTitle().trim(), e);
+        }
     }
 
     @Override
