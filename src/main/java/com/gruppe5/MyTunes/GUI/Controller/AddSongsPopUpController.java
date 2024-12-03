4cd5741fb373634fc06eeecec57535ba7c382651
Lucas <94841588+UsernameLuxi@users.noreply.github.com>
2024-12-03 12:35:15 +0100

    File picker song add

--- src/main/java/com/gruppe5/MyTunes/GUI/Controller/AddSongsPopUpController.java
+++ src/main/java/com/gruppe5/MyTunes/GUI/Controller/AddSongsPopUpController.java
@@ -3,6 +3,10 @@ package com.gruppe5.MyTunes.GUI.Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class AddSongsPopUpController {
     private MyTunesController parent;
@@ -10,6 +14,8 @@ public class AddSongsPopUpController {
     @FXML
     private TextField txtName;
 
+    @FXML
+    private TextField txtFile;
 
     /**
      * Sets a reference to the main window controller (parent window)
@@ -21,5 +27,15 @@ public class AddSongsPopUpController {
 
     @FXML
     private void onFileChoose(ActionEvent actionEvent) {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Choose a file");
+        chooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Wav Files", "*.wav"),
+                new FileChooser.ExtensionFilter("Mp3 Files", "*.mp3")
+        );
+        File file = chooser.showOpenDialog(null);
+        if (file != null) {
+            txtFile.setText(file.getPath());
+        }
     }
 }
