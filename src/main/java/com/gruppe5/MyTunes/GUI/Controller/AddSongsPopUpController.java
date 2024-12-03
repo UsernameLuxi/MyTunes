b1c5a79368071939e5a9beb4b84d4569a2836996
Casper <casfro01@easv365.dk>
2024-12-03 12:36:51 +0100

    nye ting

--- src/main/java/com/gruppe5/MyTunes/GUI/Controller/AddSongsPopUpController.java
+++ src/main/java/com/gruppe5/MyTunes/GUI/Controller/AddSongsPopUpController.java
@@ -2,6 +2,8 @@ package com.gruppe5.MyTunes.GUI.Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
@@ -16,6 +18,22 @@ public class AddSongsPopUpController {
 
     @FXML
     private TextField txtFile;
+    @FXML
+    private Button btnSave;
+    @FXML
+    private Button btnCancel;
+    @FXML
+    private Button btnFile;
+    @FXML
+    private Button btnCategory;
+    @FXML
+    private TextField txtTime;
+    @FXML
+    private MenuButton mbtnCategory;
+    @FXML
+    private TextField txtTitle;
+    @FXML
+    private TextField txtArtist;
 
     /**
      * Sets a reference to the main window controller (parent window)
@@ -37,5 +55,9 @@ public class AddSongsPopUpController {
         if (file != null) {
             txtFile.setText(file.getPath());
         }
+    }
+
+    @FXML
+    private void onCancel(ActionEvent actionEvent) {
     }
 }
