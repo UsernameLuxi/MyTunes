3eb66033b85614560c31739143ec7335100b80ea
Casper <casfro01@easv365.dk>
2024-12-04 09:31:30 +0100

    Display when the program is unhappy with the user

--- src/main/java/com/gruppe5/MyTunes/GUI/Controller/NewGenreController.java
+++ src/main/java/com/gruppe5/MyTunes/GUI/Controller/NewGenreController.java
@@ -2,6 +2,7 @@ package com.gruppe5.MyTunes.GUI.Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -16,9 +17,9 @@ public class NewGenreController {
     }
 
     @FXML
-    private void onAdd(ActionEvent actionEvent) throws Exception {
+    private void onAdd(ActionEvent actionEvent) {
         if (genreInput.getText().trim().isEmpty()) {
-            throw new Exception("Genre Name cannot be empty");
+            displayError(new Exception("Genre Name cannot be empty"));
         }
         else{
             parent.addGenre(genreInput.getText());
@@ -30,5 +31,13 @@ public class NewGenreController {
     private void onCancel(ActionEvent actionEvent) {
         Stage stage = (Stage) genreInput.getScene().getWindow();
         stage.close();
+    }
+
+    private void displayError(Throwable t)
+    {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Something went wrong");
+        alert.setHeaderText(t.getMessage());
+        alert.showAndWait();
     }
 }
